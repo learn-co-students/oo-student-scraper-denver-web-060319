a56cd5e4eb67de5e9ef5065e391fe7cc6cce465a
Blake A. Long <blake.a.long@gmail.com>
2019-12-21 17:02:34 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,66 @@ require 'pry'
 
 class Scraper
 
+
+  #name: doc.css(".student-card a .student-name, h4").first.text
+  #location: doc.css(".student-card a .student-location, p").first.text
+  #profile_url: doc.css("div .student-card a").attribute("href").value
+  
   def self.scrape_index_page(index_url)
-    
-  end
+    students = Nokogiri::HTML(open(index_url))
 
+    student_array = Array.new
+    
+    students.css(".roster-cards-container .student-card").map do |student|
+      
+      student_hash = {}
+      
+      student_hash[:name] = student.css(".student-card a .student-name, h4").first.text
+      student_hash[:location] = student.css(".student-card a .student-location, p").first.text
+      student_hash[:profile_url] = student.css("a").attribute("href").value
+      
+      student_array << student_hash
+    end
+    student_array
+  end
+      
+  
+  
   def self.scrape_profile_page(profile_url)
-    
-  end
+    student = Nokogiri::HTML(open(profile_url))
 
+    student_hash = Hash.new
+
+    links = student.css(".social-icon-container").children.css("a").map { |link| link.attribute("href").value }
+ 
+      # binding.pry
+      links.map do | link | 
+        if link.include?("twitter") 
+          student_hash[:twitter] = link
+        elsif link.include?("linkedin")
+            student_hash[:linkedin] = link
+        elsif link.include?("github")
+            student_hash[:github] = link
+        else 
+          student_hash[:blog] = link 
+        end
+      end
+      
+      
+      if student.css(".profile-quote").first.text 
+        student_hash[:profile_quote] = student.css(".profile-quote").first.text 
+      end
+      if student.css(".description-holder p").text 
+        student_hash[:bio] = student.css(".description-holder p").text 
+      end
+      
+    # end
+    # binding.pry
+    student_hash
+  end
+ 
+      
+    
 end
+  
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+    save
+  end
+  
+  def save
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.map { |student_hash| Student.new(student_hash) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.map do |key, value|
+      self.send(("#{key}="), value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
